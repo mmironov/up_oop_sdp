6a7197bbb110ca347dd848ce72a32029c203af31
Miroslav <miroslavmironov@gmail.com>
2015-10-22 17:48:51 +0300

    Initial Commit

---
+++ UP_OOP_SDP/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  UP_OOP_SDP
+//
+//  Created by Miroslav Mironov on 10/22/15.
+//  Copyright © 2015 Miroslav Mironov. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
